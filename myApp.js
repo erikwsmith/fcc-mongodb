4c0cbb5226678076e5ea02bd3c73d7d9dbfd1007
Erik Smith <erikwsmith85@gmail.com>
2025-04-19 19:32:03 +0000

    feat: Added mongodb connection

--- myApp.js
+++ myApp.js
@@ -5,8 +5,8 @@ let express = require('express');
 let app = express();
 
 console.log(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true })
 
-//mongoose.connect([process.env.MONGO_URI], {useNewUrlParser: true, useUnifiedTopology: true })
 let homePage = __dirname + '/views/index.html'
 
 //output index.html at '/' route
